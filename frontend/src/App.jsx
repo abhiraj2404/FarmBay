211219dadc8729c7e3dc55daaf8dd9eceb839584
rohan099dubey <rohan03dubey@gmail.com>
2024-08-22 23:26:36 +0530

    changed heading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <h1 className='bg-red-500 text-xl'> hello world</h1>
+        <h1 className="bg-red-500 text-xl"> Farmbay</h1>
       </div>
-     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
